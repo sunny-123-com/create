fcee85bba3dbac52510e753017ceaf3b9f5104c0
sunny-123-com <spandananalla123@gmail.com>
2024-10-19 18:39:50 +0530

    create

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let h1=document.createElement("h1")
+h1.innerText="hello world"
 document.body.appendChild(h1)
 let p=document.getElementById("hello")
+p.innerText="hello"
 document.body.appendChild(p)
